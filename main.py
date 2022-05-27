2ae007ffe94c5af51afd4e281731cf5c77d8c4a8
Muhammed9535 <105399837+Muhammed9535@users.noreply.github.com>
2022-05-27 18:37:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+
+
+first_word = input("Enter your first word or sentence: ")
+second_word = input("Enter your second word or sentence: ")
+def find_anagram(first, secon):
+    if len(first_word) == len(second_word) and sorted(first_word) == sorted(second_word):
+        return True
+    else:
+        return False
+
+
+print(find_anagram(first_word,second_word))
+
+
+
+ 
